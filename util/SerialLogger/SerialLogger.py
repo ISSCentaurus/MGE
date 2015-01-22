c35361c92b6e911fbadcf613ed34eae6d4b9e800
Sam. <s@robcuthbertson.com>
2015-01-22 12:53:34 -0700

    Update SerialLogger.py

--- util/SerialLogger/SerialLogger.py
+++ util/SerialLogger/SerialLogger.py
@@ -2,13 +2,15 @@ import serial
 import csv
 
 ser = serial.Serial()
+millis = 0
 port = input('COM Port [10]: ')  or 10
 destination = input('Output File [./data.csv]: ') or './data.csv'
 ser.port = int(port)
 ser.open()
-with open(destination, 'w', encoding='utf8') as f:
+with open(destination, 'w', encoding='utf8', newline='') as f:
     writer = csv.writer(f)    
     while True:
-        str = ser.readline().decode("utf-8")
+        str = ser.readline()
+        millis += 1
         print(str)
-        writer.writerow([str,])
+        writer.writerow([str,millis])
